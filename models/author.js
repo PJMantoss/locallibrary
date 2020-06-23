81f1477b0e62e141f14c86efe75e7dab0d6a8c71
pjmantoss <joelptoss@gmail.com>
2020-06-23 12:41:30 +0100

    update author model

--- models/author.js
+++ models/author.js
@@ -4,9 +4,9 @@ let Schema = mongoose.Schema;
 
 let AuthorSchema = new Schema(
     {
-        first_name: {},
-        family_name: {},
-        date_of_birth: {},
-        date_of_date: {}
+        first_name: {type: String, required: true, maxlength: 100},
+        family_name: {type: String, required: true, maxlength: 100},
+        date_of_birth: {type: Date},
+        date_of_date: {type: Date}
     }
 )
