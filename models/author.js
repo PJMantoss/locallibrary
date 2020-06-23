5fddf7f1a9cf251acd5fceb72b4fa581c9806acd
pjmantoss <joelptoss@gmail.com>
2020-06-23 13:05:40 +0100

    update author model

--- models/author.js
+++ models/author.js
@@ -7,14 +7,14 @@ let AuthorSchema = new Schema(
         first_name: {type: String, required: true, maxlength: 100},
         family_name: {type: String, required: true, maxlength: 100},
         date_of_birth: {type: Date},
-        date_of_date: {type: Date}
+        date_of_death: {type: Date}
     }
 )
 
 //Virtual for Author's full name
 AuthorSchema.virtual('name').get(function(){
     /*
-    To avoid errors in cases where an author does not have either a family name or first name
+    To avoid errors in cases where an author does not have either a family name or first name.
     We want to make sure we handle the exception by returning an empty string for that case
     */
 
@@ -27,4 +27,14 @@ AuthorSchema.virtual('name').get(function(){
     }
 
     return fullname;
+});
+
+//Virtual for Author's lifespan
+AuthorSchema.virtual('lifespan').get(function(){
+    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
 })
+
+//Virtual for Author's URL
+AuthorSchema.virtual('url').get(function(){
+    return '/catalog/author/' + this._id;
+});
