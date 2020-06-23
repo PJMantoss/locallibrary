6933e9d75825fd5d966cf61aaa70ad92699ce674
pjmantoss <joelptoss@gmail.com>
2020-06-23 12:39:19 +0100

    add models

---
+++ models/author.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let AuthorSchema = new Schema(
+    {
+        first_name: {},
+        family_name: {},
+        date_of_birth: {},
+        date_of_date: {}
+    }
+)
