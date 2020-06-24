3d626f9a9c6834887eee37a77a57fcb96abadd98
pjmantoss <joelptoss@gmail.com>
2020-06-24 17:01:59 +0100

    update bookInstance model

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -4,7 +4,7 @@ let Schema = mongoose.Schema;
 
 let BookInstanceSchema = new Schema(
     {
-        book: {type: Schema.Types.ObjectId, ref: 'Book', required: true},
+        book: {type: Schema.Types.ObjectId, ref: 'Book', required: true}, //Reference to the associated book
         imprint: {type: String, required: true},
         status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved']},
         due_back: {type: Date, default: Date.now}
