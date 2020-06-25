f048abc84739c71d84c05eb2009378fee76eee85
pjmantoss <joelptoss@gmail.com>
2020-06-25 13:11:24 +0100

    update catalog route

--- routes/catalog.js
+++ routes/catalog.js
@@ -62,3 +62,11 @@ router.get('/author/:id', author_controller.author_detail);
 
 // GET request for list of all Authors.
 router.get('/authors', author_controller.author_list);
+
+// GENRE ROUTES //
+
+// GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
+router.get('/genre/create', genre_controller.genre_create_get);
+
+//POST request for creating Genre.
+router.post('/genre/create', genre_controller.genre_create_post);
