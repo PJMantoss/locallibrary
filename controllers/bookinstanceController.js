cf533dd4b39a6bcabc5be9ddb9ff55ad7d3c343d
pjmantoss <joelptoss@gmail.com>
2020-06-25 12:02:10 +0100

    update BookInstance controller

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -10,7 +10,22 @@ exports.bookinstance_detail = function(req, res){
     res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id)
 }
 
-//Display Book instance create form on GET
+//Display BookInstance create form on GET
 exports.bookinstance_create_get = function(req, res){
     res.send('NOT IMPLEMENTED: BookInstance create GET')
+}
+
+//Handle BookInstance create on POST
+exports.bookinstance_create_post = function(req, res){
+    res.send('NOT IMPLEMENTED: BookInstance create POST')
+}
+
+//Display BookInstance delete form on GET
+exports.bookinstance_delete_get = function(req, res){
+    res.send('NOT IMPLEMENTED: BookInstance delete GET')
+}
+
+//Handle BookInstance delete on POST
+exports.bookinstance_delete_post = function(req, res){
+    res.send('NOT IMPLEMENTED: BookInstance delete POST')
 }
