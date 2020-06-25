35a4c1d4f2aadb983c588718cd38ea4f3214f384
pjmantoss <joelptoss@gmail.com>
2020-06-25 11:38:23 +0100

    update author controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -19,3 +19,18 @@ exports.author_create_get = function(req, res){
 exports.author_create_post = function(req, res){
     res.send('NOT IMPLEMENTED: Author create POST')
 }
+
+//Display Author delete form on GET
+exports.author_delete_get = function(req, res){
+    res.send('NOT IMPLEMENTED: Author delete GET')
+}
+
+//Handle Author delete on POST
+exports.author_delete_post = function(req, res){
+    res.send('NOT IMPLEMENTED: Author delete POST')
+}
+
+//Display Author update form on GET
+exports.author_update_get = function(req, res){
+    res.send('NOT IMPLEMENTED: Author update GET')
+}
