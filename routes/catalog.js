0d5e9c29eda303d6b1029399422a037cfd531e94
pjmantoss <joelptoss@gmail.com>
2020-06-25 12:50:03 +0100

    update catalog route

--- routes/catalog.js
+++ routes/catalog.js
@@ -18,3 +18,15 @@ router.get('/book/create', book_controller.book_create_get);
 
 // POST request for creating Book.
 router.post('/book/create', book_controller.book_create_post);
+
+// GET request to delete Book.
+router.get('/book/:id/delete', book_controller.book_delete_get);
+
+// POST request to delete Book.
+router.post('/book/:id/delete', book_controller.book_delete_post);
+
+// GET request to update Book.
+router.get('/book/:id/update', book_controller.book_update_get);
+
+// POST request to update Book.
+router.get('/book/:id/update', book_controller.book_update_post);
