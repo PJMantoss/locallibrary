13842d86ab2a8dbff908168c66cc7a6a2aa1b95a
pjmantoss <joelptoss@gmail.com>
2020-06-25 13:42:34 +0100

    update index.js in route

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let cataLogRouter = require('./routes/catalog') //Import routes for 'catalog' area of site
 
-var app = express();
+let app = express();
 
 //set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Joel2:kingofglory@cluster0-sffs5.mongodb.net/local_library?retryWrites=true&w=majority';
+let mongoose = require('mongoose');
+let mongoDB = 'mongodb+srv://Joel2:kingofglory@cluster0-sffs5.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
