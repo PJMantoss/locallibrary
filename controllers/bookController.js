929b476e46aacd5e15e8dcd760d3515a5677e9fa
pjmantoss <joelptoss@gmail.com>
2020-06-25 12:18:17 +0100

    update BookInstance controller

---
+++ controllers/bookController.js
@@ -0,0 +1,21 @@
+let Book = require('../models/book');
+
+//Display list of all Books
+exports.book_list = function(req, res){
+    res.send('NOT IMPLEMENTED: Book list')
+}
+
+//Display detail page for a specific Book
+exports.book_detail = function(req, res){
+    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id)
+}
+
+//Display Book create form on GET
+exports.book_create_get = function(req, res){
+    res.send('NOT IMPLEMENTED: Book create GET')
+}
+
+//Handle Book create on POST
+exports.book_create_post = function(req, res){
+    res.send('NOT IMPLEMENTED: Book create POST')
+}
