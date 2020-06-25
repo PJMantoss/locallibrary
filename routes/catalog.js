654b0533b2909eb86213a0ff49c900f0b65f76a7
pjmantoss <joelptoss@gmail.com>
2020-06-25 13:06:58 +0100

    update catalog route

--- routes/catalog.js
+++ routes/catalog.js
@@ -44,3 +44,19 @@ router.get('/author/create', author_controller.author_create_get);
 
 // POST request for creating Author.
 router.post('/author/create', author_controller.author_create_post);
+
+// GET request to delete Author.
+router.get('/author/:id/delete', author_controller.author_delete_get);
+
+// POST request to delete Author.
+router.post('/author/:id/delete', author_controller.author_delete_post);
+
+// GET request to update Author.
+router.get('/author/:id/update', author_controller.author_update_get);
+
+// POST request to update Author.
+router.post('/author/:id/update', author_controller.author_update_post);
+
+// GET request for one Author.
+router.get('/author/:id', author_controller.author_detail);
+
