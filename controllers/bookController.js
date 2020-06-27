51cd0bdde301af8ce5e53569f4320852b5608ae6
pjmantoss <joelptoss@gmail.com>
2020-06-27 15:47:51 +0100

    update Book controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -32,7 +32,7 @@ exports.book_list = function(req, res, next){
     Book.find({}, 'title author').populate('author').exec(function(err, list_books){
         if(err){return next(err);};
         //If successful, render
-        res.render()
+        res.render('book_list', {title: 'Book List', book_list: list_books});
     });
 }
 
