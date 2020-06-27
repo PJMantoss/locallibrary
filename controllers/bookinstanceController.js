a0008404bf2585bf7be9c973371ae78c1a3d3e1c
pjmantoss <joelptoss@gmail.com>
2020-06-27 16:38:45 +0100

    update BookInstance controller

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -6,9 +6,13 @@ exports.bookinstance_list = function(req, res){
 }
 
 //Display detail page for a specific Book instance
-exports.bookinstance_detail = function(req, res){
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id)
-}
+exports.bookinstance_detail = function(req, res, next){
+    BookInstance.find().populate('book').exec(function(err, list_bookinstances){
+        if(err){return next(err);}
+        //Successful, so render
+        res.render('bookinstance_list', {title: 'Book Instance List', bookinstance_list: list_bookinstances})
+    });
+};
 
 //Display BookInstance create form on GET
 exports.bookinstance_create_get = function(req, res){
