79eac655b19d51cbc024dcb9ba1591c3a569220f
pjmantoss <joelptoss@gmail.com>
2020-06-28 18:12:16 +0100

    update bookinstance controller

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -11,8 +11,19 @@ exports.bookinstance_list = function(req, res, next){
 }
 
 //Display detail page for a specific Book instance
-exports.bookinstance_detail = function(req, res){
-    res.send('NOT IMPLEMENTED: BookInstance list: ' + req.params.id)
+exports.bookinstance_detail = function(req, res, next){
+    BookInstance.findById(req.params.id)
+        .populate('book')
+        .exec(function(err, bookinstance){
+            if(err){return next(err);}
+            if(bookinstance == null){
+                let err = new Error('Book copy not found');
+                err.status == 404;
+                return next(err);
+            }
+            //Succesful, so render
+            res.render('bookinstance_detail',{ title: 'Copy: '+bookinstance.book.title, bookinstance: bookinstance })
+        })
 };
 
 //Display BookInstance create form on GET
