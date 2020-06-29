f28149128af7e5942a6c422a4f04dbac6791332e
pjmantoss <joelptoss@gmail.com>
2020-06-29 16:32:06 +0100

    update genre controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -73,6 +73,13 @@ exports.genre_create_post = function(req, res){
         if(!errors.isEmpty()){
             //There are errors. Render the form again with sanitized values/error messages.
             res.render('genre_form', {title: 'Create Genre', genre: genre, errors: errors.array()});
+            return;
+        }
+        else{
+            //Data is valid
+            //Check if Genre with same name already exists
+            Genre.findOne({name: req.body.name})
+                .exec(function(err, found_genre){});
         }
     }
 }
