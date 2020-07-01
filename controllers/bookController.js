219db6a031122bb02007561396625445a084c4ef
pjmantoss <joelptoss@gmail.com>
2020-07-01 17:29:04 +0100

    udate book controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -139,7 +139,20 @@ exports.book_create_post = [
                 },
             }, function(err, results){
                 if(err){ return next(err); }
-            })
+
+                // Mark our selected genres as checked.
+                for (let i = 0; i < results.genres.length; i++) {
+                    if (book.genre.indexOf(results.genres[i]._id) > -1) {
+                        results.genres[i].checked='true';
+                    }
+                }
+                res.render('book_form', { title: 'Create Book', authors: results.authors, genres: results.genres, book: book, errors: errors.array() });
+            });
+            return;
+        }
+        else {
+            // Data from form is valid. Save book.
+            book.save(function(err){})
         }
     }
 ];
