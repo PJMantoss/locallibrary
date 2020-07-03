d908f7359e8399ac03129b474f1ed1dfa7d1490d
pjmantoss <joelptoss@gmail.com>
2020-07-03 13:09:02 +0100

    update mongoose connection in app.js

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ let app = express();
 
 //set up mongoose connection
 let mongoose = require('mongoose');
-let mongoDB = 'mongodb+srv://Joel2:kingofglory@cluster0-sffs5.mongodb.net/local_library?retryWrites=true&w=majority';
+let dev_db_url = 'mongodb+srv://Joel2:kingofglory@cluster0-sffs5.mongodb.net/local_library?retryWrites=true&w=majority';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
